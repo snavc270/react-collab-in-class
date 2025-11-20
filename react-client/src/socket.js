503b95749a261f2d0f4f0a832e4671f5eeee7528
Courtney Snavely <courtneysnavely@Courtneys-MBP.lan>
2025-11-19 19:23:03 -0500

    updated github url

--- react-client/src/socket.js
+++ react-client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // Replace this with your Render backend URL:
-export const socket = io("https://localhost:3000", {
+export const socket = io("https://react-collab-in-class.onrender.com", {
   transports: ["websocket"],
 });
